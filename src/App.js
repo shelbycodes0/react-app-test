30a7e9baa98ac96e2b6ce7968e359885e0527b08
Shelby Courtney <shelbycodes@gmail.com>
2022-06-01 14:43:29 -0400

    added axios and API integration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   let [keyWord, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyWord(event) {
     setKeyword(event.target.value);
     console.log(keyWord);
   }
-
   return (
     <div className="App">
       <form onSubmit={search}>

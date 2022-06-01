dbd73486e7204ec0e2abc5167ce0c210c674b5af
Shelby Courtney <shelbycodes@gmail.com>
2022-05-31 23:34:43 -0400

    debug image

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "./witchHat.png";
 
 export default function Header() {
-  let magicHat = `https://cdn-icons.flaticon.com/png/512/2579/premium/2579236.png?token=exp=1654053428~hmac=a9c20d92453bfc1bf37f6be4a2913c74`;
   return (
     <div className="Header">
-      <img src={magicHat} alt="magic hat" />
+      <img
+        src="https://cdn-icons.flaticon.com/png/512/2579/premium/2579236.png?token=exp=1654055160~hmac=b6df3340ed5ed787462e1c56ca0a0d5c"
+        alt="magic hat"
+      />
       <h1>Magic Dictionary</h1>
     </div>
   );

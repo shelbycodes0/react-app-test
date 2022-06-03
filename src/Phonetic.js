35484f726bde4b505d9871c1f7e07c75c9b8d05d
Shelby Courtney <shelbycodes@gmail.com>
2022-06-03 17:46:30 -0400

    added phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank">
+          Listen
+        </a>{" "}
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
